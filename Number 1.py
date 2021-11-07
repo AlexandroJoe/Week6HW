86e1cf6018ae605f82239d942d1546b6c231180c
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2021-11-07 11:32:22 +0700

    revision

--- Number 1.py
+++ Number 1.py
@@ -1,8 +1,8 @@
 lang  = {
-    'Go' : 'A statically typed, compiled programming language designed at Google', 
+    'Java': 'A high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible',  
     'C++':'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or C with Classes',
     'C':'C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system', 
-    'Java': 'A high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible',  
+    'Go' : 'A statically typed, compiled programming language designed at Google', 
     'Swift': 'Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. and the open-source community'
 }
 
